01a5c650ccc67a980b57b1eb46bc0e8dad4df048
dreamlineproduction <info@dreamlineproduction.in>
2021-04-10 02:50:49 +0530

    app url

--- admin/resources/views/new_senior_financing.blade.php
+++ admin/resources/views/new_senior_financing.blade.php
@@ -33,32 +33,32 @@
 
 
 
-<script src="/assets/plugins/moment/min/moment.min.js"></script>
-<script src="/assets/plugins/bootstrap-daterangepicker/daterangepicker.js"></script>
-<script src="/assets/plugins/bootstrap-timepicker/js/bootstrap-timepicker.min.js"></script>
-<script src="/assets/plugins/bootstrap-slider/dist/bootstrap-slider.min.js"></script>
-<script src="/assets/plugins/bootstrap-3-typeahead/bootstrap3-typeahead.js"></script>
-<script src="/assets/plugins/jquery.maskedinput/src/jquery.maskedinput.js"></script>
-<script src="/assets/plugins/tag-it/js/tag-it.min.js"></script>
-<script src="/assets/plugins/blueimp-file-upload/js/vendor/jquery.ui.widget.js"></script>
-<script src="/assets/plugins/blueimp-tmpl/js/tmpl.min.js"></script>
-<script src="/assets/plugins/blueimp-load-image/js/load-image.all.min.js"></script>
-<script src="/assets/plugins/blueimp-canvas-to-blob/js/canvas-to-blob.min.js"></script>
-<script src="/assets/plugins/blueimp-gallery/js/jquery.blueimp-gallery.min.js"></script>
-<script src="/assets/plugins/blueimp-file-upload/js/jquery.iframe-transport.js"></script>
-<script src="/assets/plugins/blueimp-file-upload/js/jquery.fileupload.js"></script>
-<script src="/assets/plugins/blueimp-file-upload/js/jquery.fileupload-process.js"></script>
-<script src="/assets/plugins/blueimp-file-upload/js/jquery.fileupload-image.js"></script>
-<script src="/assets/plugins/blueimp-file-upload/js/jquery.fileupload-audio.js"></script>
-<script src="/assets/plugins/blueimp-file-upload/js/jquery.fileupload-video.js"></script>
-<script src="/assets/plugins/blueimp-file-upload/js/jquery.fileupload-validate.js"></script>
-<script src="/assets/plugins/blueimp-file-upload/js/jquery.fileupload-ui.js"></script>
-<script src="/assets/plugins/summernote/dist/summernote-lite.min.js"></script>
-<script src="/assets/plugins/spectrum-colorpicker2/dist/spectrum.min.js"></script>
-<script src="/assets/plugins/select-picker/dist/picker.min.js"></script>
-<script src="/assets/plugins/highlight.js/highlight.min.js"></script>
-<script src="/assets/js/demo/highlightjs.demo.js"></script>
-<script src="/assets/js/demo/form-plugins.demo.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/moment/min/moment.min.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/bootstrap-daterangepicker/daterangepicker.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/bootstrap-timepicker/js/bootstrap-timepicker.min.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/bootstrap-slider/dist/bootstrap-slider.min.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/bootstrap-3-typeahead/bootstrap3-typeahead.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/jquery.maskedinput/src/jquery.maskedinput.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/tag-it/js/tag-it.min.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/blueimp-file-upload/js/vendor/jquery.ui.widget.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/blueimp-tmpl/js/tmpl.min.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/blueimp-load-image/js/load-image.all.min.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/blueimp-canvas-to-blob/js/canvas-to-blob.min.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/blueimp-gallery/js/jquery.blueimp-gallery.min.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/blueimp-file-upload/js/jquery.iframe-transport.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/blueimp-file-upload/js/jquery.fileupload.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/blueimp-file-upload/js/jquery.fileupload-process.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/blueimp-file-upload/js/jquery.fileupload-image.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/blueimp-file-upload/js/jquery.fileupload-audio.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/blueimp-file-upload/js/jquery.fileupload-video.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/blueimp-file-upload/js/jquery.fileupload-validate.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/blueimp-file-upload/js/jquery.fileupload-ui.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/summernote/dist/summernote-lite.min.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/spectrum-colorpicker2/dist/spectrum.min.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/select-picker/dist/picker.min.js"></script>
+<script src="{{env('APP_URL')}}/assets/plugins/highlight.js/highlight.min.js"></script>
+<script src="{{env('APP_URL')}}/assets/js/demo/highlightjs.demo.js"></script>
+<script src="{{env('APP_URL')}}/assets/js/demo/form-plugins.demo.js"></script>
 
 
 @endpush
